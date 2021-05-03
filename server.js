6dc893e64f877f929396fe490c48c5780c79ef22
katliebig <liebig.kat@gmail.com>
2021-05-03 11:16:41 +0200

    finished code along

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import data from "./data.json"
 
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
@@ -16,6 +17,27 @@ app.use(bodyParser.json())
 // Start defining your routes here
 app.get('/', (req, res) => {
   res.send('Hello world')
+})
+
+app.get('/nominations', (req, res) => {
+  res.json(data)
+})
+
+app.get("/year/:year", (req, res) => {
+  const year = Number(req.params.year)
+  const showWinning = req.query.winning
+  let nominationsFromYear = data.filter((item) => item.year_award === year)
+
+  if (showWinning) {
+    nominationsFromYear = nominationsFromYear.filter((item) => item.win)
+  }
+
+  res.json(nominationsFromYear)
+})
+
+app.get("/nominations/winning", (req, res) => {
+  const winningNominations = data.filter((item) => item.win)
+  res.json(winningNominations)
 })
 
 // Start the server
